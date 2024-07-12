556bdd170d43ea9832d54a80869a519f363f2654
Lokesh <lokesh.loki.codes@gmail.com>
2024-07-12 23:03:18 +0530

    app & index file updated with redux code

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,121 @@
+import {Fragment, useEffect} from "react";
+// import {BrowserRouter as Router} from "react-router-dom"; 
+import {Routes, Route, Navigate, useLocation} from "react-router-dom";
+import PrivateRoute from "./PrivateRoute.js";
+
+import { useSelector, useDispatch } from "react-redux";
+import {getUser} from "./redux/userSlice/apiCalls";
+import {getPlaylists} from "./redux/playlistSlice/apiCalls";
+
+import Main from "./Pages/Main";
+import SignUp from "./Pages/SignUp/index.jsx";
+import Login from "./Pages/Login";
+import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home/index.jsx";
+import Library from "./Pages/Library";
+import Playlist from "./Pages/Playlist";
+import Search from "./Pages/Search";
+import LikedSongs from "./Pages/LikedSongs";
+import Profile from "./Pages/Profile";
+
+import Sidebar from "./Components/Sidebar";
+import Navbar from "./Components/Navbar";
+import AudioPlayer from "./Components/AudioPlayer";
+
+import "./app.css";
 import './output.css';
 
-function App() {
+const App = () => {
+
+  const dispatch = useDispatch();
+    
+  const location = useLocation();
+  const {user} = useSelector((state) => state.auth);
+  const {currentSong} = useSelector((state) => state.audioPlayer);
+
+  useEffect(() => {
+    let token = null;
+    const root = JSON.parse(window.localStorage.getItem("persist:root"));
+
+    if (root) {
+      const {auth} = root;
+      const {user} = JSON.parse(auth);
+      if (user) token = user.token;
+    }
+
+    if (user && token) {
+      getUser(user._id, dispatch);
+      getPlaylists(dispatch);
+    }
+  }, [dispatch, user]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p class="text-3xl font-bold underline">Hello lokify</p>
-      </header>
-    </div>
+    
+    <Fragment>
+
+      {user &&
+        location.pathname !== "/login" &&
+        location.pathname !== "/" &&
+        location.pathname !== "/signup" &&
+        location.pathname !== "/not-found" && (
+          
+          <Fragment>
+            <Navbar />
+            <Sidebar />
+            { currentSong && <AudioPlayer /> }
+          </Fragment>
+        )
+      }
+
+    {/* <Router> */}
+
+      <Routes>
+
+          <Route exact path="/" element={<Main />} />
+
+          <Route
+                exact user={user}
+                path="/home"
+                element={<PrivateRoute component={Home} />} />
+          <Route 
+                exact user={user}
+                path="/collection/tracks"
+                element={<PrivateRoute component={LikedSongs} />} />
+          <Route
+                exact user={user}
+                path="/collection/playlists"
+                element={<PrivateRoute component={Library} />} />
+          <Route
+                exact user={user}
+                path="/search"
+                element={<PrivateRoute component={Search} />} />
+          <Route
+                exact user={user}
+                path="/playlist/:id"
+                element={<PrivateRoute component={Playlist} />} />
+          <Route
+                exact user={user}
+                path="/me"
+                element={<PrivateRoute component={Profile} />} />
+
+          {/* {user && <Route path="/signup" element={ <Navigate to="/home" /> } />} */}
+          {user && <Route path="/login" element={ <Navigate to="/home" />} />}
+
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/not-found" element={<NotFound />} />
+          
+          <Route path="/not-found" element={ <Navigate to="/not-found" /> } />
+
+        </Routes>
+      
+      {/* </Router> */}
+    
+    </Fragment>
+  
   );
-}
+
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,24 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+
+import { Provider } from 'react-redux';
+import {PersistGate} from "redux-persist";
+import {persistStore} from "redux-persist";
+import store from "./redux/store.js";
+
 import App from './App';
-import './output.css';
+import './app.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+
+let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <ToastContainer
-        position='bottom-center'
-        autoClose={2000}
-        hideProgressBar={true}
-        closeButton={false}
-        theme="colored"
-        icon={false}
-      />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <ToastContainer
+            position='bottom-center'
+            autoClose={2000}
+            hideProgressBar={true}
+            closeButton={false}
+            theme="colored"
+            icon={false}
+          />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
